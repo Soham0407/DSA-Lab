7f0be5b2309103e73fcbed87413af14114a76135
Soham0407 <soham040707@.com>
2026-01-06 16:04:11 +0530

    DSA lab

---
+++ Lab-1/1-Entering_array(RMO).c
@@ -0,0 +1,9 @@
+//program for entering array as a row major and print it
+#include <stdio.h>
+int main()
+{
+    printf("Enter number of rows and columns: ");
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int arr[r][c];
+}
